8c7045ab36869f98b3d2cbdd7db76007ac4a03e2
satishPanigrahi <satish.panigrahi@gmail.com>
2016-03-18 13:49:06 -0300

    Update a previous mistake TabBarController.swift

---
+++ WeightWatch/WeightWatch/TabBarController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarController.swift
+//  WeightWatch
+//
+//  Created by Satish Chandra Panigrahi on 2016-03-18.
+//  Copyright © 2016 Satish Chandra Panigrahi. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
