0d7c7923a5777a349098558e1ef786c2e3416fb2
satishPanigrahi <satish.panigrahi@gmail.com>
2016-03-18 12:47:36 -0300

    (1) Set Status status bar Hidden (2) create a custom class TabBarController

---
+++ WeightWatch/WeightWatch/TabBarControllerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarControllerViewController.swift
+//  WeightWatch
+//
+//  Created by Satish Chandra Panigrahi on 2016-03-18.
+//  Copyright © 2016 Satish Chandra Panigrahi. All rights reserved.
+//
+
+import UIKit
+
+class TabBarControllerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
