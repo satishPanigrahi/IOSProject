6a74a8de5518609ca955de40ae0ad12f9457e7b8
satishPanigrahi <satish.panigrahi@gmail.com>
2016-03-19 20:47:01 -0300

    Shows your current weight and  BMI

--- WeightWatch/WeightWatch/FirstViewController.swift
+++ WeightWatch/WeightWatch/FirstViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet var labelUnits: UILabel!
+    @IBOutlet var labelcurrentWeight: UILabel!
+    @IBOutlet var Photo: UIImageView!
     @IBOutlet weak var labelBMI: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.labelBMI.text = "Current BMI: \(Float(User.currentUser.BMI))"
-        
+        self.Photo.image = UIImage(named: "imageBMI.png")
+        //unitsControl.addTarget(self, action: "unitsChanged:", forControlEvents: .ValueChanged)
+        self.labelcurrentWeight.text = "Your current weight is: \(Float(User.currentUser.weight))"
+        self.labelUnits.text = User.currentUser.units
     }
 
     override func didReceiveMemoryWarning() {
