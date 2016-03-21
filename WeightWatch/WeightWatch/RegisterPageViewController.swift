9d46f7eb3102dca15fbc5ead4506a1da9d0516a1
satishPanigrahi <satish.panigrahi@gmail.com>
2016-03-21 11:05:43 -0300

    1) Added validation to register page 2) store email and password

---
+++ WeightWatch/WeightWatch/RegisterPageViewController.swift
@@ -0,0 +1,79 @@
+//
+//  RegisterPageViewController.swift
+//  WeightWatch
+//
+//  Created by Satish Chandra Panigrahi on 2016-03-21.
+//  Copyright © 2016 Satish Chandra Panigrahi. All rights reserved.
+//
+
+import UIKit
+
+class RegisterPageViewController: UIViewController {
+    @IBOutlet var userEmailTextField: UITextField!
+    @IBOutlet var userPasswordTextField: UITextField!
+    @IBOutlet var userRepeatPasswordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func RegisterButtonPressed(sender: AnyObject) {
+        let userEmail = userEmailTextField.text
+        let userPassword = userPasswordTextField.text
+        let userRpeatPassword = userRepeatPasswordTextField.text
+        
+        // check for empty field
+        
+        if (userEmail!.isEmpty || userPassword!.isEmpty || userRpeatPassword!.isEmpty){
+            //dispaly error msg
+            displayMyAlertMsg("All Fileds are required")
+            return;
+        }
+        
+        // check for password match
+        
+        if (userPasswordTextField != userRepeatPasswordTextField){
+            //display error msg
+            displayMyAlertMsg("Passwords do not match")
+            return;
+            
+        }
+        
+        // Store data
+        
+        NSUserDefaults.standardUserDefaults().setObject(userEmail, forKey: "userEmail")
+        NSUserDefaults.standardUserDefaults().setObject(userPassword, forKey: "userPassword")
+        NSUserDefaults.standardUserDefaults().synchronize();
+        
+        // Dispaly alert msg for confirmation
+        
+    }
+    
+    func displayMyAlertMsg(UserMsg:String){
+        let myAlert = UIAlertController(title: "Alert", message: UserMsg, preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+        
+        myAlert.addAction(okAction)
+        self.presentViewController(myAlert, animated: true, completion: nil);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
